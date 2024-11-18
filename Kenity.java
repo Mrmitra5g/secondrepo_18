061965904a4dc0fed7fb0aaf806aea08bde85008
mrmitra5g <mrmitra678@gmail.com>
2024-11-18 15:24:19 +0530

    new branch code

---
+++ Kenity.java
@@ -0,0 +1,14 @@
+public class Kenity {
+    public static void main(String[] args) {
+        
+
+        int serv=78;
+
+        System.out.println(serv ++);
+
+        for (String string : args) {
+            System.out.println( string );
+        }
+    }
+    
+}
